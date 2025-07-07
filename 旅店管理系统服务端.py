5199f4b3bca93a423d47cecca26f34bb880bb5e6
xiaodaiameng <aaaaa1_2024@qq.com>
2025-07-08 01:17:48 +0800

    update_seperateCode

--- 旅店管理系统服务端.py
+++ 旅店管理系统服务端.py
@@ -5,6 +5,8 @@ import pymysql
 import threading
 from functools import wraps #导入装饰器,方便对连接函数的多次调用
 
+import adminManager
+
 DB_CONFIG = {
     'host':'localhost','user':'root','password':'123456',
     'database':'hotel_digital_library','charset':'utf8mb4'
@@ -102,40 +104,7 @@ if __name__ == "__main__":
     init_database()
 name = ""
 
-class AdminManager:
-    """管理员功能封装类"""
-    @staticmethod
-    @db_connection_handler
-    def _9_authenticate(username,password,conn=None):
-        """管理员认证"""
-        try:
-            with conn.cursor() as cursor:
-                cursor.execute("SELECT password FROM managers_table WHERE username = %s", (username,))
-                result = cursor.fetchone()  #result是一个元组
-                if result and result[0] == password:
-                    return True
-        except pymysql.Error as e:
-            print(f'管理员认证错误:{e}')
-            return False
-    @staticmethod
-    @db_connection_handler
-    def data_presentation(client_socket,conn=None):
-        """显示所有表数据给管理员所在客户端"""
-        try:
-            with conn.cursor() as cursor:
-                tables={'管理员表':'managers_table','用户表':'customers_table','房间表':'rooms_table'}
-                for name, table in tables.items():
-                    cursor.execute(f"SELECT * FROM {table}")
-                    result = cursor.fetchall()
-                    handle_send(client_socket, f"{name}:\n{result}\n")
-                return True
-        except pymysql.Error as e:
-            handle_send(client_socket, f"查询表数据错误:{e}")
-            return False
-    @staticmethod
-    def admin_menu(client_socket):
-        """管理员菜单"""
-        handle_send(client_socket,"请输入您要增删改查的类型:\t1.客户\t2.房间\t0.退出\t请选择:")
+
 class CustomerManager:
     """客户管理功能"""
     @staticmethod
